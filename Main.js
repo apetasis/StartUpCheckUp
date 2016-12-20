49542dc524f396484137948bcbaa04012c4ab782
apetasis <ampetasis@gmail.com>
2016-12-19 16:50:49 -0800

    Added JavaScript Validation
    Added JavaScript Validation

--- Main.js
+++ Main.js
@@ -2,3 +2,12 @@ function pdfCreate(x) {
     GrabzIt("YWFmNTY4Yjc1NjNmNGY0YThmNjc5N2Q2NTFlOTFlNTg=").ConvertHTML(x, {"format": "pdf", "background": 0}).Create();
 } 
 
+function validateCheckBoxes() {
+	if(letter_info.purpose1.checked == false && letter_info.purpose2.checked == false && letter_info.purpose3.checked == false) {
+		document.getElementById("no_purpose").innerHTML = "Please check one of the purpose boxes";
+		return false;
+	}
+	else {
+		return true;
+	}
+}
