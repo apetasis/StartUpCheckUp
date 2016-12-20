68614e71b0715643c7ec3bd32ebef97fc722392b
apetasis <ampetasis@gmail.com>
2016-12-19 17:45:04 -0800

    Debug additions
    12/19/16

--- Main.js
+++ Main.js
@@ -2,7 +2,16 @@ function pdfCreate(x) {
     GrabzIt("YWFmNTY4Yjc1NjNmNGY0YThmNjc5N2Q2NTFlOTFlNTg=").ConvertHTML(x, {"format": "pdf", "background": 0}).Create();
 } 
 
-function validateCheckBoxes() {
+function validateForm() {
+	
+	if ($.get("administrative_area") !== "Delaware" || $.get("administrative_area") !== "DE" ) {
+		alert("Needs to be a Delaware address");
+		return false;
+	}
+
+	else {
+		return true;
+	}
 	if(letter_info.purpose1.checked == false && letter_info.purpose2.checked == false && letter_info.purpose3.checked == false) {
 		document.getElementById("no_purpose").innerHTML = "Please check one of the purpose boxes";
 		return false;
@@ -10,4 +19,5 @@ function validateCheckBoxes() {
 	else {
 		return true;
 	}
+
 }
